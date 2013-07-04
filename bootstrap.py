fba770fe165947f3a00f3d59b5496f4aa4d3e854
Don Najd <dnajd7@gmail.com>
2013-07-04 00:01:56 -0700

    choregraphe behaviors working again; updated box library

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,6 @@
 import naoutil.naoenv as naoenv
+import naoutil.memory as memory
 import fluentnao.nao as nao
+
 env = naoenv.make_environment(None, ipaddr="nao.local", port=9559)
 nao = nao.Nao(env, None)
