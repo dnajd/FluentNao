0443d7daad8fc14ce321c9309954e62374fd9863
Don Najd <dnajd7@gmail.com>
2013-07-14 22:59:05 -0700

    added broker to boostrap to allow for events in interactive prompt

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,14 @@
 import naoutil.naoenv as naoenv
 import naoutil.memory as memory
 import fluentnao.nao as nao
+from datetime import datetime
+from naoutil import broker
 
-env = naoenv.make_environment(None, ipaddr="nao.local", port=9559)
+# broker (must come first)
+broker.Broker('bootstrapBroker', naoIp="nao.local", naoPort=9559)
+
+# nao env
+env = naoenv.make_environment(None) #using broker don't need ->, ipaddr="nao.local", port=9559)
+
+# fluent nao
 nao = nao.Nao(env, None)
